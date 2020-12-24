216f2703b71b6b5c58181768b7c16e8b002ec486
Helen <esum29@yandex.ru>
2020-12-24 07:59:55 +0300

    Изменен текстовый документ

---
+++ Library/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using System.Runtime;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сложение: ");
+            double resultAdd = Class1.Add(4, 53);
+            Console.WriteLine(resultAdd);
+            Console.ReadLine();
+
+            Console.WriteLine("Вычитание: ");
+            double resultSub = Class1.Substract(24, 5);
+            Console.WriteLine(resultSub);
+            Console.ReadLine();
+
+            Console.WriteLine("Умножение: ");
+            double resultMul = Class1.Multiply(2555, 2);
+            Console.WriteLine(resultMul);
+            Console.ReadLine();
+
+            Console.WriteLine("Деление: ");
+            double resultDiv = Class1.Divide(64, 8);
+            Console.WriteLine(resultDiv);
+            Console.ReadLine();
+        }
+    }
+}
+
